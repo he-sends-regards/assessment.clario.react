c6300a24e7192927d9d95f7a2149f6d983b00c9c
Ivan L <postscriptum.no@gmail.com>
2024-08-03 15:21:06 +0300

    [INIT]: initialise vite + react-ts project; [STYLE]: add colors palette and Inter font

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+export const COLORS = {
+  TITLE_TEXT: "#4A4E71",
+  COMMON_TEXT: "#4A4E71",
+  INPUT: {
+    PLACEHOLDER: "#6F91BC",
+    BORDER: {
+      FOCUS: "#6F91BC",
+    },
+  },
+  BUTTON: {
+    BACKGROUND_LINEAR: {
+      FROM: "#70C3FF",
+      TO: "#4B65FF",
+    },
+    TEXT: "#FFFFFF",
+  },
+};
